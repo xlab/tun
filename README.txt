dbba9a9519601c3389253c8cc7373a4c641b87fc
Maxim Kouprianov <maxim@kouprianov.com>
2014-08-06 03:11:36 +0400

    List entrypoints (only 1 for now).

--- README.txt
+++ README.txt
@@ -3,3 +3,4 @@ Spawns an http server that serves files from the specified directory.
 
 OPTIONS:
   -p=5051: a port used by the server
+  -v=false: be verbosive
--- tun.go
+++ tun.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
 )
 
-var port int
+var (
+	port    int
+	verbose bool
+)
 
 func init() {
 	flag.IntVar(&port, "p", 5051, "a port used by the server")
+	flag.BoolVar(&verbose, "v", false, "be verbosive")
 	flag.Usage = func() {
 		name := os.Args[0]
 		fmt.Fprintf(os.Stderr, "Usage: %s [option] <dir>\n", name)
@@ -28,14 +34,37 @@ func main() {
 		flag.Usage()
 		os.Exit(-1)
 	}
-	name := flag.Arg(0)
-	if info, err := os.Stat(name); err != nil {
+	port := strconv.Itoa(port)
+	dir := flag.Arg(0)
+	if info, err := os.Stat(dir); err != nil {
 		log.Fatalln(err)
 	} else if !info.IsDir() {
-		log.Fatalf("stat %s: not a directory", name)
+		log.Fatalf("stat %s: not a directory", dir)
 	}
-	http.Handle("/", http.FileServer(http.Dir(name)))
-	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
+	if verbose {
+		if ip, err := getIP(); err != nil {
+			log.Fatalln("unable to list entrypoints:", err)
+		} else {
+			println("entrypoint:", "http://"+ip.String()+":"+port)
+		}
+	}
+	http.Handle("/", http.FileServer(http.Dir(dir)))
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func getIP() (addr net.IP, err error) {
+	var addrs []net.Addr
+	if addrs, err = net.InterfaceAddrs(); err != nil {
+		return
+	}
+	for i := range addrs {
+		if ipnet, ok := addrs[i].(*net.IPNet); ok {
+			if ipnet.IP.IsGlobalUnicast() && ipnet.IP.To4() != nil {
+				return ipnet.IP, nil
+			}
+		}
+	}
+	return nil, errors.New("no public ip found")
 }
