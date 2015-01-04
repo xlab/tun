46fa110c6c0326e2a108b0ac25bf0a918dc8a62b
Maxim Kouprianov <max@kc.vc>
2015-01-04 17:52:40 +0300

    Improved docker image building scheme.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM progrium/busybox
 MAINTAINER Maxim Kupriianov <max@kc.vc>
 ADD https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt /etc/ssl/ca-bundle.pem
 
-ADD snapshot/linux_amd64/tun /go/bin/tun
+ADD bin/tun /go/bin/tun
 ENTRYPOINT ["/go/bin/tun"]
 EXPOSE 5051
--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-WORKSPACE=$GOPATH/src/github.com/xlab
-PACKAGE=$WORKSPACE/tun
-DOCK=$WORKSPACE/tun
-NAME=tun
+EXEC=tun
+PACKAGE=github.com/xlab/tun
+WORKDIR=$GOPATH/src/$PACKAGE
+DOCKOUT=$WORKDIR/docker
 
-# compile the binary
-goxc -arch amd64 -os linux -wd $PACKAGE -d $DOCK compile
+# compile the binary (uncomment the option)
+# goxc -q -arch amd64 -os linux -wd $WORKDIR -o="$DOCKOUT/bin/$EXEC" compile
+# gobldock -o $DOCKOUT/bin/$EXEC $PACKAGE
+exit 0
+
 # build the docker image
-docker build -t xlab/$NAME $DOCK
+cp $WORKDIR/Dockerfile $DOCKOUT/Dockerfile
+docker build -t xlab/$EXEC $DOCKOUT
 # save docker image as tar archive
-docker save -o $DOCK/$NAME.tar xlab/$NAME
+docker save -o $DOCKOUT/$EXEC.tar xlab/$EXEC
 # compress the archive
-gzip -f $DOCK/$NAME.tar
+gzip -f $DOCKOUT/$EXEC.tar
